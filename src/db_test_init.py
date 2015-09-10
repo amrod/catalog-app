50fc1767e6ee9d928456c059488c8d668941da5d
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-09-09 20:36:16 -0400

    Add test data init script

--- src/db_test_init.py
+++ src/db_test_init.py
@@ -1,5 +1,6 @@
 import inventoryapp
-from inventoryapp.models import User
+from datetime import datetime
+from inventoryapp.models import User, Mask, Transaction
 
 if __name__ == '__main__':
 
@@ -8,23 +9,36 @@ if __name__ == '__main__':
              {'name': 'TestUser 3', 'email': 'test3@example.org'},
              {'name': 'TestUser 4', 'email': 'test4@example.net'}]
 
-    masks = [{'name': 'CARD1', 'form_factor': '2FF', 'quantity': 10, 'user_id': 1},
-             {'name': 'CARD2', 'form_factor': '3FF', 'quantity': 40, 'user_id': 2},
-             {'name': 'CARD3', 'form_factor': '4FF', 'quantity': 70, 'user_id': 3},
-             {'name': 'CARD4', 'form_factor': 'NANO', 'quantity': 20, 'user_id': 4}]
+    masks = [{'user_id': 2,'name': 'CARD1', 'form_factor': '2FF', 'quantity': 10},
+             {'user_id': 3,'name': 'CARD2', 'form_factor': '3FF', 'quantity': 40},
+             {'user_id': 4,'name': 'CARD3', 'form_factor': '4FF', 'quantity': 70},
+             {'user_id': 5,'name': 'CARD4', 'form_factor': 'NANO', 'quantity': 20}]
 
-    txs = [{'user_id': 1,'mask_id': 0, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 20, 'date': '2015/1/6'},
-           {'user_id': 2,'mask_id': 1, 'description': 'Development', 'source': 'loc1', 'destination': 'loc2', 'quantity': 30, 'date': '2015/9/1'},
-           {'user_id': 3,'mask_id': 2, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 50, 'date': '2015/1/5'},
-           {'user_id': 4,'mask_id': 3, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 10, 'date': '2015/1/2'},
-           {'user_id': 4,'mask_id': 3, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 80, 'date': '2015/3/1'},]
+    txs = [{'user_id': 2,'mask_id': 1, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 20, 'date': datetime(2015, 1, 6)},
+           {'user_id': 2,'mask_id': 1, 'description': 'Development', 'source': 'loc1', 'destination': 'loc2', 'quantity': 30, 'date': datetime(2015, 9, 1)},
+           {'user_id': 3,'mask_id': 2, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 50, 'date': datetime(2015, 1, 5)},
+           {'user_id': 4,'mask_id': 3, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 10, 'date': datetime(2015, 1, 2)},
+           {'user_id': 4,'mask_id': 3, 'description': 'New cards', 'source': 'loc1', 'destination': 'loc2', 'quantity': 80, 'date': datetime(2015, 3, 1)},]
 
     db = inventoryapp.db
     db.create_all()
 
-    data = masks + users + txs
-    for kwargs in data:
+    for kwargs in users:
+        print kwargs
         u = User(**kwargs)
         db.session.add(u)
         db.session.commit()
+
+    for kwargs in masks:
+        print kwargs
+        m = Mask(**kwargs)
+        db.session.add(m)
+        db.session.commit()
+
+    for kwargs in txs:
+        print kwargs
+        t = Transaction(**kwargs)
+        db.session.add(t)
+        db.session.commit()
+
 
