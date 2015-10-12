e079dad59ca8a9e1a2a228bd3bfd5c33fbb732f9
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-10-12 04:39:32 -0400

    Update config with public parameters

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
+import random
+import string
+
 DEBUG = False
 SQLALCHEMY_ECHO = False
 BCRYPT_LEVEL = 12  # Configuration for the Flask-Bcrypt extension
+SECRET_KEY = ''.join(random.choice(string.ascii_letters +  string.digits) for x in xrange(32))
 UPLOAD_FOLDER = 'uploads'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///recipecatalog.db'
