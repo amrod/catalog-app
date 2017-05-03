a2870ea770ec596234042c345ebaeab8ac5d5f2a
Amaury Rodriguez <amrod@users.noreply.github.com>
2017-05-03 12:24:13 -0400

    Add crypto libraries

--- pg_config.sh
+++ pg_config.sh
@@ -3,6 +3,10 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
+apt-get -qqy install python-dev 
+apt-get -qqy install libffi-dev 
+apt-get -qqy install libssl-dev 
+
 pip install oauth2client==1.5.1
 pip install requests==2.2.1
 pip install httplib2==0.9.2
